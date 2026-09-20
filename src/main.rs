251cb5bc56e3aea1c5eff483d6efafb54d033885
deucenn <panix.black@web.de>
2026-09-20 12:26:12 +0200

    build: cli logic

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,33 @@ use std::fs;
 use std::fs::File;
 use std::io::Write;
 use serde::{Deserialize, Serialize};
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "rask")]
+struct Args {
+    // rask command you wanna use
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    // add todo
+    add {
+        title: String,
+    },
+    // list all todos
+    list,
+    // change todo status
+    done {
+        id: u8,
+    },
+    // delete todo              
+    delete {
+        id: u8,
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Todo {
@@ -12,9 +39,12 @@ struct Todo {
 } 
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // get args from cli
+    let args = Args::parse();
+
     let file_path = "saves.json";
 
-    // init json file
+    // get data from json file, if no data new Vec
     let mut todos: Vec<Todo> = if fs::metadata(file_path).is_ok() {
         let content = fs::read_to_string(file_path)?;
         serde_json::from_str(&content).unwrap_or_default()
@@ -22,21 +52,59 @@ fn main() -> Result<(), Box<dyn Error>> {
         Vec::new()
     };
     
-    println!("{:?}", todos);
+    // handle cli args
+    match &args.command {
+        Commands::add{title} => {
+            todos.push(Todo {
+                id: (todos.len() as u8) + 1,
+                title: title.clone(),
+                done: false,
+            });
+        }
+        Commands::list => {
+            if todos.is_empty() {
+                println!("no todos in this list");
+            } else {
+                println!("your todos:");
+                for todo in &todos {
+                    let status = if todo.done {
+                        "Done"
+                    } else {
+                        "Not done"
+                    };
+                    println!("id: {}: {}, status: {}", todo.id, todo.title, status);
+                }
+            }
+        }
+        Commands::done{id} => {
+            let index: usize = (id - 1).into();
+            let mut todo_found: bool = false;
+            for todo in &todos {
+                if *id == todo.id {
+                    todo_found = true;
+                }
+            };
 
-    let new_todo = Todo {
-        id: (todos.len() as u8) + 1,
-        title: "Work".to_string(),
-        done: false,
-    };
-
-    todos.push(new_todo);
+            if todo_found == false {
+                println!("cant find todo {}", id);
+            } else {
+                if todos[index].done == false {
+                    todos[index].done = true;
+                }
+            }
+        }
+        Commands::delete{id} => {
+            let index: usize = (id - 1).into();
+            todos.remove(index);
+            println!("todo {} got removed", id);
+        }
+    }
 
     let json_data = serde_json::to_string_pretty(&todos)?;
     
     // save todos
     fs::write(file_path, json_data);
-    println!("Json Data is written");
+    println!("Updated your todos.");
     
     Ok(())
 }
